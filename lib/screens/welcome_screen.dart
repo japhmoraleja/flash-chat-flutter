de390658459b0dac2eeb801c2cffe63dc33eb250
Kaiser Nalle <japhet.moraleja@gmail.com>
2020-08-17 21:45:50 +0800

    Cleaned Code

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
 import 'registration_screen.dart';
-// import 'package:flash_chat/utils/transitions.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -22,21 +21,12 @@ class _WelcomeScreenState extends State<WelcomeScreen> with TickerProviderStateM
     controller = AnimationController(
       duration: Duration(seconds: 4),
       vsync: this,
-      // upperBound: 0.60,
     );
 
     animation = ColorTween(begin: Colors.blue, end: Colors.white).animate(controller);
-
-    // animation = CurvedAnimation(
-    //   parent: controller,
-    //   curve: Curves.easeIn,
-    // );
-
     controller.forward();
-
     controller.addListener(() {
       setState(() {});
-      print(animation.value);
     });
   }
 
@@ -49,75 +39,106 @@ class _WelcomeScreenState extends State<WelcomeScreen> with TickerProviderStateM
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: animation.value,
+      backgroundColor: Colors.white,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Row(
-              children: <Widget>[
-                Hero(
-                  tag: 'flash-logo',
-                  child: Container(
-                    child: Image.asset('images/logo.png'),
-                    height: 60,
-                  ),
-                ),
-                TypewriterAnimatedTextKit(
-                  speed: Duration(milliseconds: 300),
-                  pause: Duration(milliseconds: 2000),
-                  totalRepeatCount: 4,
-                  text: ['Flash Chat'],
-                  textStyle: TextStyle(
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w900,
-                    color: Colors.black87,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+            FlashChatHeaderText(),
+            SizedBox(height: 48.0),
+            LogInButton(),
+            RegisterButton(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class FlashChatHeaderText extends StatelessWidget {
+  const FlashChatHeaderText({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Hero(
+          tag: 'flash-logo',
+          child: Container(
+            child: Image.asset('images/logo.png'),
+            height: 60,
+          ),
+        ),
+        TypewriterAnimatedTextKit(
+          speed: Duration(milliseconds: 300),
+          pause: Duration(milliseconds: 2000),
+          totalRepeatCount: 4,
+          text: ['Flash Chat'],
+          textStyle: TextStyle(
+            fontSize: 45.0,
+            fontWeight: FontWeight.w900,
+            color: Colors.black87,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class RegisterButton extends StatelessWidget {
+  const RegisterButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        color: Colors.blueAccent,
+        borderRadius: BorderRadius.circular(30.0),
+        elevation: 5.0,
+        child: MaterialButton(
+          onPressed: () {
+            Navigator.pushNamed(context, RegistrationScreen.id);
+          },
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            'Register',
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LogInButton extends StatelessWidget {
+  const LogInButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: Colors.lightBlueAccent,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: () {
+            Navigator.pushNamed(context, LoginScreen.id);
+          },
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            'Log In',
+          ),
         ),
       ),
     );
